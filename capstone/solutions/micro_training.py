ffad0b970097c3b8e1d1c955aa48458048344b1c
SongShawn <SongShawn@users.noreply.github.com>
2019-01-17 10:56:45 +0800

    win

--- capstone/solutions/micro_training.py
+++ capstone/solutions/micro_training.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import time
 import datetime
+import numpy as np
 
 # 取出训练集中的预测目标字段
-feature_names = ['Year', 'Month', 'Day', 'Hour', 'DayOfWeekID', 'PdDistrictID', 'HasBlock', 'PositionTypeID', 'X', 'Y']
+feature_names = ['Year', 'Month', 'Hour', 'DayOfWeekID', 'PdDistrictID', 'HasBlock', 'PositionTypeID', 'X', 'Y']
 
 train_data = pd.read_csv("../datasets/train_preprocess.csv")
 # sample_data = train_data.sample(frac = 0.01, random_state=10)
@@ -20,7 +21,7 @@ from sklearn.preprocessing import LabelEncoder
 LabelEncTarget = LabelEncoder()
 target = LabelEncTarget.fit_transform(target)
 
-X_train, X_test, y_train, y_test = 
+X_train, X_test, y_train, y_test = \
     train_test_split(features, target, test_size=0.2, random_state=42)
 print('X_train has {} samples.'.format(X_train.shape[0]))
 print('X_test has {} samples.'.format(X_test.shape[0]))
@@ -48,20 +49,37 @@ param['max_delta_step'] = 2
 
 evallist = [(DTrain_X, 'train'), (DTest_X, 'Test')]
 
-num_round = 5000
+num_round = 3000
 early_stop = 5
 
 etas = [0.01]
 # etas = [0.2]
 start = time.time()
 model_name = "../models/model_eta_0.3.model"
+bst = None
 for i,eta in enumerate(etas) :
     param['eta'] = eta
     bst = xgb.train(param, DTrain_X, num_boost_round=num_round, 
                 evals = evallist, early_stopping_rounds=early_stop,
                 xgb_model=None)
-    model_name = '../models/model_eta_0.01_' + str(eta) + '.model'
+    model_name = '../models/model_eta_0.01_3000_' + str(eta) + '.model'
     bst.save_model(model_name)
     print('{}.th traning, eta: {}, best_score: {}'.format(i, eta, bst.best_score))
 
 print('CPU hist Trainig Time: %s seconds.' % (str(time.time() - start)))
+
+
+# predict
+start = time.time()
+valid_data = pd.read_csv("../datasets/test_preprocess.csv")
+
+X_valid = valid_data[feature_names]
+DX_valid = xgb.DMatrix(data=X_valid)
+
+y_pred_prob = np.round(bst.predict(DX_valid), 4)
+
+csv_output = pd.DataFrame(columns=LabelEncTarget.classes_, data=y_pred_prob)
+csv_output.insert(0, 'Id', valid_data['Id'])
+csv_output.to_csv('../results/model_eta_0.01_3000_result.csv', index=False)
+
+print('CPU Predict Time: %s seconds.' % (str(time.time() - start)))
